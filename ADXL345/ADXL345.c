4f629724386d90535655cd93155720eca61b69d3
Adrian <adriann0@users.noreply.github.com>
2014-11-23 20:59:44 +0100

    Dodany sterownik akcelerometru

---
+++ ADXL345/ADXL345.c
@@ -0,0 +1,71 @@
+/*
+ * ADXL345.c
+ *
+ *  Created on: 22 lis 2014
+ *      Author: adrian
+ */
+
+#include "ADXL345.h"
+#include "../I2C/i2cmaster.h"
+
+#define TO_READ 6
+
+
+char writeTo(char device_address, char address, char val)
+{
+	if(i2c_start(device_address | I2C_WRITE) == 0 && i2c_write(address) == 0 && i2c_write(val) == 0)
+	{
+		i2c_stop();
+		return 1; //sukces
+	}
+	else
+	{
+		i2c_stop();
+		return 0; //porazka
+	}
+
+
+}
+
+int powerOn(char device_address)
+{
+  gains[0] = 0.00376390;
+  gains[1] = 0.00376009;
+  gains[2] = 0.00349265;
+
+  writeTo(device_address, ADXL345_POWER_CTL, 0);
+  writeTo(device_address, ADXL345_POWER_CTL, 16);
+  writeTo(device_address, ADXL345_POWER_CTL, 8);
+}
+
+
+void readFrom(char device_address, char address, int num, char buff[])
+{
+	for(int i = 0; i < num; i++)
+	{
+		i2c_start_wait(device_address | I2C_WRITE);
+		i2c_write(address + i);
+		i2c_rep_start(device_address | I2C_READ);
+		buff[i] = i2c_readNak();
+	}
+
+	i2c_stop();
+}
+
+void readAccel(char device_address, int *x, int *y, int *z)
+{
+  readFrom(device_address, ADXL345_DATAX0, TO_READ, _buff);
+
+  *x = (((int)_buff[1]) << 8) | _buff[0];
+  *y = (((int)_buff[3]) << 8) | _buff[2];
+  *z = (((int)_buff[5]) << 8) | _buff[4];
+}
+
+void get_Gxyz(char device_address, double *xyz)
+{
+  readAccel(device_address, (int*)xyz, (int*)(xyz + 1), (int*)(xyz + 2));
+  for(int i = 0; i < 3; i++)
+  {
+	  *(xyz + i) *= gains[i];
+  }
+}
