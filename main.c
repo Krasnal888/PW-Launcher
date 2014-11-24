61cbfd8149ff530dfd13fda954cdb264df8a2cbe
Adrian <adriann0@users.noreply.github.com>
2014-11-24 20:48:09 +0100

    Dopisane odczyty (do sprawdzenia i poprawienia)

--- main.c
+++ main.c
@@ -8,13 +8,92 @@
 #include "I2C/i2cmaster.h"
 #include "adc/adc.h"
 
+//Do zapisu na SD
+FATFS FatFs;	//Ustawienie systemu plików
+FIL Dump;		//Do obsługi otwartego pliku
+
+//Do termometru DS18x20
+uint8_t czujniki_cnt;	//liczba czujników
+uint8_t subzero;	//określenie + czy - temp
+uint8_t cel;		//Temperatura
+uint8_t cel_fract_bits;	//Wartość po przecinku temperatury
+
+#define CISNIENIE1 ADC0
+#define CISNIENIE2 ADC1
+
+#define AKCELEROMETR1 (0x53 << 1)
+#define AKCELEROMETR2 (0x1D << 1)
+
 //Deklaracje funkcji
 
 
 //Main
 int main(void){
+	ADC_init(); //init barometrow
+	Acc_powerOn(AKCELEROMETR1);
+	Acc_powerOn(AKCELEROMETR2);
 
+	czujniki_cnt = search_sensors();
+	UINT bw;	//Bajty zapisane
+	char bufor[32];
 
+	double akcelerometr1_odczyty[3];
+	double akcelerometr2_odczyty[3];
+
+	while(1)
+	{
+		f_mount(&FatFs,"",0);
+		f_open(&Dump,"Test.txt",FA_WRITE | FA_OPEN_ALWAYS);
+
+		int bar1 = ADC_read(CISNIENIE1);
+		int bar2 = ADC_read(CISNIENIE2);
+		DS18X20_start_meas( DS18X20_POWER_EXTERN, NULL ); // termometry
+		Acc_get_Gxyz(AKCELEROMETR1, akcelerometr1_odczyty);
+		Acc_get_Gxyz(AKCELEROMETR2, akcelerometr2_odczyty);
+
+		sprintf(bufor,"A1 %f %f %f A2 %f %f %f",
+				akcelerometr1_odczyty[0], akcelerometr1_odczyty[1], akcelerometr1_odczyty[2],
+				akcelerometr2_odczyty[0], akcelerometr2_odczyty[1], akcelerometr2_odczyty[2]);
+		f_lseek(&Dump,f_size(&Dump));
+		f_write(&Dump,bufor,sizeof(bufor),&bw);
+		f_sync(&Dump);
+
+		sprintf(bufor,"C1 %d C2 %d",
+						bar1,
+						bar2);
+		f_lseek(&Dump,f_size(&Dump));
+		f_write(&Dump,bufor,sizeof(bufor),&bw);
+		f_sync(&Dump);
+
+		for(int sensor_numer = 0; sensor_numer < czujniki_cnt; sensor_numer++)
+		{
+			if(DS18X20_OK == DS18X20_read_meas(gSensorIDs[sensor_numer], &subzero, &cel, &cel_fract_bits))
+			{
+				if (subzero == 0){
+					sprintf(bufor," +%d,%d ",cel,cel_fract_bits);
+					f_lseek(&Dump,f_size(&Dump));
+					f_write(&Dump,bufor,7,&bw);
+					f_sync(&Dump);
+					_delay_ms(20);
+				}
+				if (subzero == 1){
+					sprintf(bufor," -%d,%d ",cel,cel_fract_bits);
+					f_lseek(&Dump,f_size(&Dump));
+					f_write(&Dump,bufor,7,&bw);
+					f_sync(&Dump);
+					_delay_ms(20);
+				}
+			}
+		}
+
+		//nowa linia
+		sprintf(bufor,"\n");
+		f_lseek(&Dump,f_size(&Dump));
+		f_write(&Dump,bufor,7,&bw);
+		f_sync(&Dump);
+		f_close(&Dump);
+
+	}
 
 }
 
