a12a37a4e682735910c861382cff2d95e8ff9353
Adrian <adriann0@users.noreply.github.com>
2014-11-28 19:56:57 +0100

    Main

--- main.c
+++ main.c
@@ -27,78 +27,83 @@ uint8_t cel_fract_bits;	//Wartość po przecinku temperatury
 #define CISNIENIE2 ADC2
 #define AKCELEROMETR1 (0x1D<<1) // 1 na SD0
 #define AKCELEROMETR2 (0x53<<1) // 0 na SD0
+#define NAZWAPLIKU "TEST.TXT"
+
+//PD7 dioda
 
 //Deklaracje funkcji
+void dopiszDoLogu(char *filename, char *text2)
+{
+	UINT bw;	//Bajty zapisane
+	f_mount(&FatFs,"",0);
+	f_open(&Dump,filename,FA_WRITE | FA_OPEN_ALWAYS);
 
+	if (f_lseek(&Dump, f_size(&Dump)) == FR_OK)
+	{
+		f_write(&Dump, text2, strlen(text2), &bw);
+	}
+	f_close(&Dump);
+}
 
 //Main
 int main(void){
-	ADC_init(); //init barometrow
-	Acc_turn_on(AKCELEROMETR1);
-	Acc_turn_on(AKCELEROMETR2);
-
-	czujniki_cnt = search_sensors();
-	UINT bw;	//Bajty zapisane
-	char bufor[32];
-
 	int akcelerometr1_odczyty[3];
 	int akcelerometr2_odczyty[3];
+	int bar1;
+	int bar2;
+
+	//Acc_turn_on(AKCELEROMETR1);
+	//Acc_turn_on(AKCELEROMETR2);
+	ADC_init(); //init barometrow
+	czujniki_cnt = search_sensors(); //init termometry
+
+	DDRD |= (1 << 7); //dioda
+
+
+	UINT bw;	//Bajty zapisane
+	char bufor[64];
 
 	while(1)
 	{
-		f_mount(&FatFs,"",0);
-		f_open(&Dump,"Test.txt",FA_WRITE | FA_OPEN_ALWAYS);
-
-		int bar1 = ADC_read(CISNIENIE1);
-		int bar2 = ADC_read(CISNIENIE2);
-		Acc_get_Gxyz(AKCELEROMETR1, akcelerometr1_odczyty);
-		Acc_get_Gxyz(AKCELEROMETR2, akcelerometr2_odczyty);
+		if(PORTD & (1 << 7))
+			PORTD &= ~(1 << 7);
+		else
+			PORTD |= (1 << 7);
 
 		DS18X20_start_meas( DS18X20_POWER_EXTERN, NULL ); // termometry
+		bar1 = ADC_read(CISNIENIE1);
+		bar2 = ADC_read(CISNIENIE2);
+		//Acc_get_Gxyz(AKCELEROMETR1, akcelerometr1_odczyty);
+		//Acc_get_Gxyz(AKCELEROMETR2, akcelerometr2_odczyty);
+
+		sprintf(bufor,"C1,%d,C2,%d",
+						bar1,
+						bar2);
+		dopiszDoLogu(NAZWAPLIKU, bufor);
+
+		/*sprintf(bufor,",A1,%d,%d,%d,A2,%d,%d,%d",
+						akcelerometr1_odczyty[0], akcelerometr1_odczyty[1], akcelerometr1_odczyty[2],
+						akcelerometr2_odczyty[0], akcelerometr2_odczyty[1], akcelerometr2_odczyty[2]);
+		dopiszDoLogu(NAZWAPLIKU, bufor);*/
 
 		_delay_ms(750);
-
-		sprintf(bufor,"A1,%d,%d,%d,A2,%d,%d,%d",
-				akcelerometr1_odczyty[0], akcelerometr1_odczyty[1], akcelerometr1_odczyty[2],
-				akcelerometr2_odczyty[0], akcelerometr2_odczyty[1], akcelerometr2_odczyty[2]);
-		f_lseek(&Dump,f_size(&Dump));
-		f_write(&Dump,bufor,sizeof(bufor),&bw);
-		f_sync(&Dump);
-
-		sprintf(bufor,",C1,%d,C2,%d",
-				bar1,
-				bar2);
-		f_lseek(&Dump,f_size(&Dump));
-		f_write(&Dump,bufor,sizeof(bufor),&bw);
-		f_sync(&Dump);
 
 		for(int sensor_numer = 0; sensor_numer < czujniki_cnt; sensor_numer++)
 		{
 			if(DS18X20_OK == DS18X20_read_meas(gSensorIDs[sensor_numer], &subzero, &cel, &cel_fract_bits))
 			{
 				if (subzero == 0){
-					sprintf(bufor,",+%d.%d",cel,cel_fract_bits);
-					f_lseek(&Dump,f_size(&Dump));
-					f_write(&Dump,bufor,7,&bw);
-					f_sync(&Dump);
-					_delay_ms(20);
+					sprintf(bufor,",ID%d,+%d.%d",gSensorIDs[sensor_numer],cel,cel_fract_bits);
+					dopiszDoLogu(NAZWAPLIKU, bufor);
+
 				}
 				if (subzero == 1){
-					sprintf(bufor,",-%d.%d",cel,cel_fract_bits);
-					f_lseek(&Dump,f_size(&Dump));
-					f_write(&Dump,bufor,7,&bw);
-					f_sync(&Dump);
-					_delay_ms(20);
+					sprintf(bufor,",ID%d,-%d.%d",gSensorIDs[sensor_numer],cel,cel_fract_bits);
+					dopiszDoLogu(NAZWAPLIKU, bufor);
 				}
 			}
 		}
-
-		//nowa linia
-		sprintf(bufor,"\n");
-		f_lseek(&Dump,f_size(&Dump));
-		f_write(&Dump,bufor,7,&bw);
-		f_sync(&Dump);
-		f_close(&Dump);
+		dopiszDoLogu(NAZWAPLIKU, "\n");
 
 	}
 
